f569c6f86dbefcb5e3e928b24d5cd64a40ab6c3a
trungdangtd <trungdang1301@gmail.com>
2023-09-25 09:10:21 +0700

    add FireBase với làm chức năng onboarding

---
+++ app/src/main/java/edu/huflit/ecapp1/OnboardingActivity.java
@@ -0,0 +1,87 @@
+package edu.huflit.ecapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+
+    Button btn;
+    SliderAdapter sliderAdapter;
+    TextView[] dots;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //giấu status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_onboarding);
+
+        //giấu toolbar
+        getSupportActionBar().hide();
+        viewPager = findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+        btn = findViewById(R.id.get_started_btn);
+
+        addDots(0);
+
+        viewPager.addOnPageChangeListener(changeListener);
+        // gọi adapter
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+    }
+
+    private void addDots(int position){
+
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+        for(int i = 0; i < dots.length; i++){
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if(dots.length > 0){
+            dots[position].setTextColor(getResources().getColor(R.color.pink));
+        }
+    }
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            addDots(position);
+
+            if(position == 0){
+
+                btn.setVisibility(View.INVISIBLE);
+
+            } else if (position ==1) {
+                btn.setVisibility(View.INVISIBLE);
+
+            }else {
+                btn.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
