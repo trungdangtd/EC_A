afb82c15ff1b1207561b17ea94a8939627cce2d6
trungdangtd <trungdang1301@gmail.com>
2023-09-25 08:05:15 +0700

    Initial commit

---
+++ app/src/main/java/edu/huflit/ecapp1/RegistrationActivity.java
@@ -0,0 +1,80 @@
+package edu.huflit.ecapp1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    EditText name,email,password;
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+    }
+    public void signup(View view) {
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if (TextUtils.isEmpty(userName))
+        {
+            Toast.makeText(this, "Enter Name!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (TextUtils.isEmpty(userEmail))
+        {
+            Toast.makeText(this, "Enter Email Address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userPassword))
+        {
+            Toast.makeText(this, "Enter Password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (userPassword.length() < 6)
+        {
+            Toast.makeText(this, "Độ dài mật khẩu quá ngắn, tối thiểu 6 ký tự", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        auth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegistrationActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                        }
+                        else {
+                            Toast.makeText(RegistrationActivity.this, "Đăng ký thất bại" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+
+    }
+
+    public void signin(View view) {
+        startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+    }
+}
